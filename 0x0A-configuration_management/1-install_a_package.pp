ac4b0df7a0e75d476199f550a52ac4219ddfd7fb
Eslam Elmishtawy <eslamelmishtawy@gmail.com>
2024-02-24 00:33:17 -0800

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
 # Install flask (2.1.0)
-exec {'install-flask':
-require => Exec['install-python'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
 
-exec {'install-python':
-command => '/usr/bin/which python3'
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
